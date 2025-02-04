a1ec729b7b0ebd07e6ce5f22c9e72ddc42f889be
yongsin-oak <64070217@it.kmitl.ac.th>
2025-02-05 01:05:05 +0700

    [Assignment] Cellule3.3 - Web

---
+++ cell03/ex03/todo.js
@@ -0,0 +1,34 @@
+const saveTasks = () => {
+  let tasks = [];
+  document
+    .querySelectorAll("#ft_list div")
+    .forEach((div) => tasks.push(div.textContent));
+  document.cookie = `tasks=${JSON.stringify(tasks)}`;
+};
+
+const addTask = (text, save = true) => {
+  const taskDiv = document.createElement("div");
+  taskDiv.textContent = text;
+  taskDiv.addEventListener("click", () => {
+    if (confirm("Do you want to remove this task?")) {
+      taskDiv.remove();
+      saveTasks();
+    }
+  });
+  const ftList = document.getElementById("ft_list");
+  ftList.prepend(taskDiv);
+  if (save) saveTasks();
+};
+
+const newTask = () => {
+  const task = prompt("Enter a new TO DO:");
+  if (task) addTask(task);
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+  const [key, saveTasks] = document.cookie.split("=");
+  if (saveTasks) {
+    const tasks = JSON.parse(saveTasks);
+    tasks.map((task) => addTask(task, false));
+  }
+});
